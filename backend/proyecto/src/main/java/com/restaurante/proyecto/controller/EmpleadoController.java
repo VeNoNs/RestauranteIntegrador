0627810acf3a55ef11c8d2102ed997c795f5e79e
Cristopher Gabriel Vera Quispe <cris.vera.quispe@gmail.com>
2024-10-27 23:11:57 -0500

    Apachecommons
    Validadores para el registro de empleados

--- backend/proyecto/src/main/java/com/restaurante/proyecto/controller/EmpleadoController.java
+++ backend/proyecto/src/main/java/com/restaurante/proyecto/controller/EmpleadoController.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import com.restaurante.proyecto.entities.Empleado;
 import com.restaurante.proyecto.service.EmpleadoService;
 import com.restaurante.proyecto.service.Impl.ExcelService; // Importar el servicio de Excel
+import org.apache.commons.validator.routines.EmailValidator; // Importar el validador de correo electrónico
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -36,8 +37,14 @@ public class EmpleadoController {
     @PostMapping("/api/nuevo")
     @ResponseBody
     public ResponseEntity<Empleado> guardarNuevoEmpleado(@RequestBody Empleado empleado) {
+        // Validar que el empleado no sea nulo
         Preconditions.checkNotNull(empleado, "El empleado no puede ser nulo.");
         
+        // Validación del correo electrónico
+        if (!EmailValidator.getInstance().isValid(empleado.getEmail())) {
+            return ResponseEntity.badRequest().body(null); 
+        }
+
         Empleado nuevoEmpleado = empleadoService.crearEmpleado(empleado);
         return ResponseEntity.ok(nuevoEmpleado);
     }
@@ -46,13 +53,14 @@ public class EmpleadoController {
     @PutMapping("/api/editar/{idEmpleado}")
     @ResponseBody
     public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long idEmpleado, @RequestBody Empleado empleado) {
+        // Validar que el ID del empleado sea positivo
         Preconditions.checkArgument(idEmpleado != null && idEmpleado > 0, "El ID del empleado debe ser un valor positivo.");
         
         Empleado empleadoActualizado = empleadoService.actualizarEmpleado(idEmpleado, empleado);
         if (empleadoActualizado != null) {
             return ResponseEntity.ok(empleadoActualizado);
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().build(); // Si no existe el empleado, devuelve un 404
         }
     }
 
@@ -60,6 +68,7 @@ public class EmpleadoController {
     @DeleteMapping("/api/eliminar/{idEmpleado}")
     @ResponseBody
     public ResponseEntity<Void> eliminarEmpleado(@PathVariable Long idEmpleado) {
+        // Validar que el ID del empleado sea positivo
         Preconditions.checkArgument(idEmpleado != null && idEmpleado > 0, "El ID del empleado debe ser un valor positivo.");
         
         empleadoService.eliminarEmpleado(idEmpleado);
